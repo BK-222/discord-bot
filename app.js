d4d8e6a4ff575e74fca07458511ed5aea60d08a2
BK-222 <letsgetjscoding@gmail.com>
2021-04-12 10:38:41 +0100

    Test

---
+++ app.js
@@ -0,0 +1,4 @@
+const express = require('express.js');
+const app = express();
+
+module.exports = app;
---
+++ beatrice.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+
+const Discord = require('discord.js');
+const client = new Discord.Client({
+  partials: ['MESSAGE']
+});
+
+client.on('ready', () => {
+  console.log('The bot is ready to go!');
+});
+
+client.on('message', (msg) => {
+  if (msg.content == 'Blonde') {
+    msg.channel.send('And bouncy!');
+    msg.reply('And bouncy!');
+  }
+  if (msg.content == 'I love Beato!') {
+    msg.react('❤️');
+  }
+});
+
+client.on('messageDeleted', (msg) => {
+  msg.channel.send('Uuuuuuuuu, what are you hiding??');
+});
+
+client.login(process.env.BOT_TOKEN);
