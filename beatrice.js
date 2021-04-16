14efdf669f844667ee416a693ccbf879344e85da
BK-222 <letsgetjscoding@gmail.com>
2021-04-16 21:49:21 +0100

    Commented out

--- beatrice.js
+++ beatrice.js
@@ -32,8 +32,8 @@ client.on('message', (msg) => {
   
   if (!message.startsWith(PREFIX) || msg.author.bot) return;
   
-  let args = msg.content.slice(PREFIX.length).trim().split(' ');
-  let command = args.shift().toLowerCase();
+  //let args = msg.content.slice(PREFIX.length).trim().split(' ');
+  //let command = args.shift().toLowerCase();
   
   
  
