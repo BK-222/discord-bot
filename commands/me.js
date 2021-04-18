923f010b5aa915f8f60a930ab2daa78c3dfdf69f
BK-222 <letsgetjscoding@gmail.com>
2021-04-18 19:32:38 +0100

    Changed me.js file to backticks

--- commands/me.js
+++ commands/me.js
@@ -1,6 +1,6 @@
 const me = {
   name: 'me',
-  description: 'displays the user\'s name and id',
+  description: `displays the user's name and id`,
   execute(msg) {
     msg.channel.send(`Forgot your name, ${msg.author.username}??`);
     msg.channel.send(`Here's also your id #${msg.author.id}`);
