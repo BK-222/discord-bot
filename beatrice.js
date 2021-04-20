bc6edae3900be15379f51265e0f7dcf3d8bdc822
BK-222 <letsgetjscoding@gmail.com>
2021-04-21 00:01:06 +0100

    Commenting out guard clause

--- beatrice.js
+++ beatrice.js
@@ -41,7 +41,7 @@ client.on('message', (msg) => {
   let args = msg.content.trim().split(/ +/);
   let command = args.shift().toLowerCase();
   
-  if (!client.commands.has(command)) return;
+  //if (!client.commands.has(command)) return;
   //let command = client.commands.get(commandName);
   
   if (command == `${PREFIX}cmd`) {
