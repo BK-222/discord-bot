5990eb8c7019f20adca28b18f838eb3fcf32dcfd
BK-222 <letsgetjscoding@gmail.com>
2021-04-23 23:17:42 +0100

    Commented in embed back.

--- commands/emb.js
+++ commands/emb.js
@@ -4,19 +4,19 @@ const emb = {
   execute(msg, args, Discord) {
     if (!args.length) {
       let embed = new Discord.MessageEmbed()
-        //.setColor('#ffffff')
-        //.setTitle('ERROR')
-        //.setDescription('Nothing here')
-        //.setFooter('Error log')
-        //.addField('Code', 'Greetings!', false)
-        //.addFields(
-          //{ name: 'Shannon', value: 'servant', //inline: false },
-          //{ name: 'Kannon', value: 'servant', //inline: false }
-        //)
-        //.setTimestamp()
-        //.setImage('https://imgur.com/a/lAetrF1')
-        //.setThumbnail('https://imgur.com/a/lAetrF1');
-      return msg.channel.send(`test`);
+        .setColor('#ffffff')
+        .setTitle('ERROR')
+        .setDescription('Nothing here')
+        .setFooter('Error log')
+        .addField('Code', 'Greetings!', false)
+        .addFields(
+          { name: 'Shannon', value: 'servant', inline: false },
+          { name: 'Kannon', value: 'servant', inline: false }
+        )
+        .setTimestamp()
+        .setImage('https://imgur.com/a/lAetrF1')
+        .setThumbnail('https://imgur.com/a/lAetrF1');
+      return msg.channel.send(embed);
     } else if (args[0] == 'none') {
       return msg.channel.send('Crickets.');
     }
